7b2a2f4ca09e887d899ab2430ee9b9c1f9c2917d
herbayulet <sayaherbayu@gmail.com>
2022-01-11 16:05:17 +0700

    Pembelajaran JS hari ke- 1

--- novice/01-01/main.js
+++ novice/01-01/main.js
@@ -1,1 +1,66 @@
-console.log("herbayu");
+var first = "hello world";
+console.log(first);
+console.log("===================");
+
+// pengenalan variabel
+let namaDepan = "Herbayu";
+let namaBelakang = "Eko Nugroho";
+
+let fullName;
+console.log((fullName = `Nama saya adalah ${namaDepan} ${namaBelakang}`));
+
+console.log("===================");
+
+//tipe data string
+var x;
+x = "John";
+
+var y;
+y = "doe";
+
+console.log(x, y);
+
+console.log("==================");
+
+let j = "Bob";
+j = "Steve";
+console.log(j);
+
+console.log("==================");
+
+// tipe data number
+let a = 8;
+let b = 5;
+let c = a + b;
+let d = a - b;
+let e = a * b;
+let f = a / b;
+let g = a % b;
+let h = (a + b) * 2;
+
+console.log(c, d, e, f, g, h);
+// Tipe data boolean
+console.log(a > b);
+console.log(e < c);
+console.log("Itchy" != "Scratchy");
+console.log("Apple" == "Orange");
+console.log("==================");
+
+console.log("ini adalah komentar");
+// saya lagi belajar javascript
+/*mohon dilancarkan dan di permudah untuk segala urusan nya terkait JS*/
+
+// tipe data array
+let mobil = ["Toyota, Honda, BMW"];
+mobil.push("Mercedez");
+console.log(mobil);
+let motor = ["Suzuki, Yamaha, Kawasaki"];
+let kendaraan = mobil.concat(motor);
+console.log(kendaraan);
+console.log("=================");
+
+// Tipe data objek
+const car = { type: "BMW", model: "500", color: "silver", price: "Rp 1.000.000.000" };
+console.log(`Saya punya mobil ${car.type} dengan model ${car.model} yang warna nya ${car.color} seharga ${car.price}`);
+
+//
