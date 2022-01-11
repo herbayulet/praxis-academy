8847e8e316d130b44d0abc345c005de9a5dcf5a9
herbayulet <sayaherbayu@gmail.com>
2022-01-11 17:00:42 +0700

    new update

--- novice/01-01/main.js
+++ novice/01-01/main.js
@@ -27,17 +27,24 @@ console.log(j);
 
 console.log("==================");
 
-// tipe data number
+// tipe data number & operator
 let a = 8;
-let b = 5;
+let b = 4;
 let c = a + b;
 let d = a - b;
 let e = a * b;
 let f = a / b;
 let g = a % b;
 let h = (a + b) * 2;
-
+if (a > b) {
+  console.log("Large");
+} else if (e < c) {
+  console.log("Small");
+} else {
+  console.log("jangan bingung");
+}
 console.log(c, d, e, f, g, h);
+
 // Tipe data boolean
 console.log(a > b);
 console.log(e < c);
@@ -56,6 +63,16 @@ console.log(mobil);
 let motor = ["Suzuki, Yamaha, Kawasaki"];
 let kendaraan = mobil.concat(motor);
 console.log(kendaraan);
+const numbers = [45, 4, 9, 16, 25];
+
+let txt = "";
+// perulangan dalam array menggunakan forEach
+numbers.forEach(myFunction);
+
+function myFunction(value) {
+  txt += value + ", ";
+}
+console.log(txt);
 console.log("=================");
 
 // Tipe data objek
