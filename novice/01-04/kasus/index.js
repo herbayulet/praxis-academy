8fe07ad30cf23f37111baccc0ee9eb9909d0ff0c
herbayulet <sayaherbayu@gmail.com>
2022-01-14 19:38:33 +0700

    new update

--- novice/01-04/kasus/index.js
+++ novice/01-04/kasus/index.js
@@ -2,9 +2,9 @@ function testEmail() {
   let myName = document.getElementById("name").value;
   let myEmail = document.getElementById("email").value;
   let myDomain = document.getElementById("domain").value;
-  let getName = myName.match().value;
+  let getName = myName;
   let getEmail = myEmail.replace("@", " ");
-  let getDomain = myDomain.match().value;
-  alert(`Halo, username anda adalah ${getName}, domain anda adalah ${getEmail}${getDomain} `);
+  // let getDomain = myDomain;
+  alert(`Halo, username anda adalah ${getName}, domain anda adalah ${getEmail}`);
   document.getElementById("form").reset();
 }
