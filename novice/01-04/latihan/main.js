1426e3b13461fefb13610b0fdb50abdcd60216b1
herbayulet <sayaherbayu@gmail.com>
2022-01-14 12:44:13 +0700

    String dan Regular Expression

--- novice/01-04/latihan/main.js
+++ novice/01-04/latihan/main.js
@@ -2,6 +2,6 @@ function calculateIdealBody() {
   let myHeight = document.getElementById("height").value;
   let yourName = document.getElementById("name").value;
   let total = myHeight - 100 - (myHeight - 100) * 0.1;
-  alert(`${yourName}, total berat ideal anda ${total}.`);
+  alert(`${yourName}, total berat ideal anda adalah ${total} kg.`);
   document.getElementById("form").reset();
 }
