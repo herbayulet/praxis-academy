ea492603e9be68135058bfff2b8cb44756597f69
herbayulet <sayaherbayu@gmail.com>
2022-01-20 17:54:57 +0700

    kobong cokk

--- novice/02-03/bintang.js
+++ novice/02-03/bintang.js
@@ -2,16 +2,30 @@ let tangga = (x) => {
   let s = "";
   for (let a = 0; a <= 5; a++) {
     for (let b = 0; b <= a; b++) {
-      s += "_";
+      s += ">";
     }
     s += "\n";
   }
   for (let i = 5; i >= x; i--) {
     for (let j = 0; j <= i; j++) {
-      s += "_";
+      s += ">";
     }
     s += "\n";
   }
   return s;
 };
 console.log(tangga(0));
+
+var s = "";
+var b = 9;
+for (var h = 1; h <= b; h++) {
+  for (var i = 1; i <= b - h; i++) {
+    s += " ";
+  }
+  for (var j = 1; j < 2 * h; j++) {
+    s += "^";
+  }
+  s += "\n";
+}
+
+console.log(s);
--- novice/02-03/forloopMasNizar.js
+++ novice/02-03/forloopMasNizar.js
@@ -1,10 +1,12 @@
+console.log("Soal Nomor 1");
 // Soal 1
 let stoper = 10;
 for (let i = 1; i <= stoper; i++) {
   console.log(i);
 }
 
-console.log("==================");
+console.log("\n");
+console.log("Soal Nomor 2");
 // Soal 2
 let total = 0;
 for (let j = 1; j <= 10; j++) {
@@ -12,7 +14,8 @@ for (let j = 1; j <= 10; j++) {
 }
 console.log(total);
 
-console.log("==================");
+console.log("\n");
+console.log("Soal Nomor 3");
 // Soal 3
 
 function menghitung() {
@@ -25,7 +28,8 @@ function menghitung() {
 }
 console.log(menghitung());
 
-console.log("==================");
+console.log("\n");
+console.log("Soal Nomor 4");
 // Soal 4
 
 let average = (x) => {
@@ -39,7 +43,8 @@ let average = (x) => {
 };
 console.log(average(10));
 
-console.log("==================");
+console.log("\n");
+console.log("Soal Nomor 5");
 // Soal 5
 
 let urutAngka = (x) => {
@@ -49,7 +54,8 @@ let urutAngka = (x) => {
 };
 urutAngka(5);
 
-console.log("==================");
+console.log("\n");
+console.log("Soal Nomor 6");
 // Soal 6
 
 let perkalian = (x, y) => {
@@ -59,7 +65,8 @@ let perkalian = (x, y) => {
 };
 perkalian(10, 15);
 
-console.log("==================");
+console.log("\n");
+console.log("Soal Nomor 7");
 // Soal 7
 
 let perkalianGanda = (n) => {
@@ -75,7 +82,8 @@ let perkalianGanda = (n) => {
 };
 perkalianGanda(8);
 
-console.log("==================");
+console.log("\n");
+console.log("Soal Nomor 8");
 // Soal 8
 
 let bilanganGanjil = (n) => {
@@ -89,7 +97,8 @@ let bilanganGanjil = (n) => {
 };
 bilanganGanjil(10);
 
-console.log("==================");
+console.log("\n");
+console.log("Soal Nomor 9");
 // Soal 9
 
 let segitigaSiku = (rows) => {
@@ -104,7 +113,8 @@ let segitigaSiku = (rows) => {
 };
 console.log(segitigaSiku(10));
 
-console.log("==================");
+console.log("\n");
+console.log("Soal Nomor 10");
 // Soal 10
 
 let sikuAngka = (x) => {
@@ -120,7 +130,8 @@ let sikuAngka = (x) => {
 };
 console.log(sikuAngka(10));
 
-console.log("==================");
+console.log("\n");
+console.log("Soal Nomor 11");
 // Soal 11
 function segitigaAngka(panjang) {
   let hasil = "";
@@ -134,37 +145,100 @@ function segitigaAngka(panjang) {
 }
 console.log(segitigaAngka(10));
 
-console.log("==================");
+console.log("\n");
+console.log("Soal Nomor 12");
 // Soal 12
 
-let baris = (rows) => {
-  let k = 1;
-  console.log(`Masukan jumlah baris: ${rows}`);
-  for (let i = 1; i <= rows; i++) {
-    for (let j = 1; j <= i; j++) console.log(k++);
-    {
-      console.log("\n");
-    }
-  }
-};
-baris(4);
-
-// console.log("==================");
-// // Soal 13
-
-// function piramid4(rows) {
-//   let t = 1;
+// let baris = (rows) => {
+//   let k = 1;
 //   console.log(`Masukan jumlah baris: ${rows}`);
-//   let spc = rows + 4 - 1;
 //   for (let i = 1; i <= rows; i++) {
-//     for (let k = spc; k > 1; k--) {
-//       console.log(" ");
-//     }
-//     for (let j = 1; j <= i; j++) {
-//       console.log(t++);
+//     for (let j = 1; j <= i; j++) console.log(k++);
+//     {
 //       console.log("\n");
-//       spc--;
 //     }
 //   }
-// }
-// piramid4(4);
+// };
+// baris(4);
+
+console.log("\n");
+console.log("Soal Nomor 13");
+// soal 13
+
+console.log("\n");
+console.log("Soal Nomor 14");
+// soal 14
+
+console.log("\n");
+console.log("Soal Nomor 15");
+// soal 15
+
+let faktor5 = (x) => {
+  let f = 1;
+  for (let i = 1; i <= x; i++) {
+    f *= i;
+  }
+  return f;
+};
+console.log(faktor5(5));
+
+console.log("\n");
+console.log("Soal Nomor 16");
+// soal 16
+
+let bilanganGenap = (x) => {
+  let sum = 0;
+  for (let i = 1; i <= x; i++) {
+    console.log(`Bilangan genap : ${2 * i}`);
+    sum += 2 * i;
+  }
+  return `Jumlah bilangan genap ke ${x} adalah ${sum}`;
+};
+console.log(bilanganGenap(5));
+
+console.log("\n");
+console.log("Soal Nomor 17");
+// soal 17
+
+console.log("\n");
+console.log("Soal Nomor 18");
+// soal 18
+
+let jumlahDeret = (n) => {
+  let sum = 1;
+  let t = 1;
+  let x = 2;
+  let d;
+  for (let i = 1; i <= n; i++) {
+    d = 2 * i * (2 * i - 1);
+    t = (-t * x * x) / d;
+    sum = sum + t;
+  }
+  // console.log(d);
+  // console.log(t);
+  console.log(`Jumlahnya : ${sum} dan hasil nya rounded up`);
+  console.log(`banyak nya suku = ${n}`);
+  console.log(`Nilai x = ${x}`);
+};
+jumlahDeret(5);
+
+console.log("\n");
+console.log("Soal Nomor 19");
+// soal 19
+
+let deretArmonik = (n) => {
+  let s = 0.0;
+  console.log(`Masukan jumlah suku : ${n}`);
+  for (let i = 1; i <= n; i++) {
+    if (i < n) {
+      console.log(`1/${i}`);
+      s += 1 / s[i];
+    } else if (i == n) {
+      console.log(`1/${i}`);
+      s += 1 / s[i];
+    }
+    // return s[i];
+  }
+  console.log(`Jumlah deret angka ${n} : ${s++}`);
+};
+deretArmonik(5);
