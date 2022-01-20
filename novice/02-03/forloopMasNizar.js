13af9ecb541ed48940bd96aa9c191806c1860b8d
herbayulet <sayaherbayu@gmail.com>
2022-01-20 11:22:03 +0700

    nested loop kacau

--- novice/02-03/forloopMasNizar.js
+++ novice/02-03/forloopMasNizar.js
@@ -74,3 +74,79 @@ let perkalianGanda = (n) => {
   }
 };
 perkalianGanda(8);
+
+console.log("==================");
+// Soal 8
+
+let bilanganGanjil = (n) => {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+    if (i % 2 == 0) {
+      console.log("bilangan genap");
+    } else {
+      console.log(i);
+    }
+  }
+};
+bilanganGanjil(10);
+
+console.log("==================");
+// Soal 9
+
+let segitigaSiku = (rows) => {
+  let txt = "";
+  for (let i = 1; i <= rows; i++) {
+    for (let j = 1; j <= i; j++) {
+      txt += "*";
+    }
+    txt += "\n";
+  }
+  return txt;
+};
+console.log(segitigaSiku(10));
+
+console.log("==================");
+// Soal 10
+
+let sikuAngka = (x) => {
+  let total = 0;
+  for (let i = 1; i <= x; i++) {
+    // console.log(i);
+    for (let j = 1; j <= i; j++) {
+      total += j;
+    }
+    total += "\n";
+  }
+  return total;
+};
+console.log(sikuAngka(10));
+
+console.log("==================");
+// Soal 11
+function segitigaAngka(panjang) {
+  let hasil = "";
+  for (let i = 1; i <= panjang; i++) {
+    for (let j = 1; j <= i; j++) {
+      hasil += i;
+    }
+    hasil += "\n";
+  }
+  return hasil;
+}
+console.log(segitigaAngka(10));
+
+console.log("==================");
+// Soal 12
+
+function segitigaPlus(panjang) {
+  let hasil = 1;
+  for (let i = 1; i <= panjang; i++) {
+    for (let j = 1; j <= i; j++) {
+      console.log(hasil++);
+    }
+    console.log("\n");
+  }
+  // return hasil;
+}
+segitigaPlus(4);
